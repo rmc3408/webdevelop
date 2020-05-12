db838d6e965c0d719dc3c3ddb8c1566a51d46a62
rmc3408 <rmc3408@protonmail.com>
2020-05-11 22:08:45 -0400

    l

---
+++ Project04 - Javascript/Function.js
@@ -0,0 +1,19 @@
+
+
+
+// function camelCasing () {   Syntax;  } 
+
+function main() {
+    lineOdd();    // function inside function
+}
+function lineOdd() { 
+    putBeeper = 2;
+}
+
+
+// function (parameters)
+function getMilk(money) {
+    var totalCost = money / 1.5;
+    console.log(Math.floor(totalCost));
+}
+getMilk(5);
