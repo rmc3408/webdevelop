fb9a972f2b2a5d4d5d43910b3e68e3b151f4503a
rmc3408 <rmc3408@protonmail.com>
2020-05-13 19:45:36 -0400

    sabe

--- Project04 - Javascript/Events.js
+++ Project04 - Javascript/Events.js
@@ -16,5 +16,105 @@ document.querySelectorAll('.drum')[i].addEventListener("click", handleClick);
     mouse            click
     keyboard         keypress event.key
     open file        load
+*/
+
+
+
+
+
+
+// Can Execute a function called playKey !!!!!
+
+document.addEventListener("keypress", playKey);
+
+function playKey() {
+    var option = event.key;
+    switch (option) {
+        case "w":
+        var audio = new Audio('sounds/tom-1.mp3');
+        audio.play();
+        break;
+    }
+}
+
+
+
+
+
+
+
+
+//---//----//---//---//---//---//---//---//
+//       CREATE Anonimous Function       //
+//---//---//---//---//---//---//---//---//
+
+/*   AddEventListener Trigger a event... by mouse or keyboard.
+     This unique event has several unique properties and values.
+
+     it will send this object properties  
+     to function named or Anonimous function.
+*/
+
+document.addEventListener("click", function() {console.log("key pressed")});
+
+//----------------- object property ARE EVENTS -> mouse or key event -------->>//
+/*    
+        You can call this specific or object event in anomimous function as argument.
+*/
+
+
+
+
+
+
+
+//---//---//---//---//---//---//---//---//---//---//---//---//---//---//
+/*
+    .addEventListener(type, High Order Function (callback Function);
+
+it take function (argument) but argument is a function
+
+*///---//---/---//---//---//---//---//---//---//---//---//---//---//---//
+
+
+// If something triggered by typeOfEvent, will trigger callback...
+function otherEventListener(userEvent, callback) {  
+    
+    // Event will create a specific object with values and property collected!!
+    var eventHappened = {
+        eventTrigger:"keypress",
+        key: 'p',
+        duration: 2
+    }
+
+    // The function callback will execute only IF 
+    //  object has property of eventType
+    if (eventHappened.eventTrigger === userEvent) {
+        callback(eventHappened);
+        
+    }
+
+}
+
+//////// -------------- USER INPUT -------------------////////
+otherEventListener("keypress", function(event) {
+    console.log(event);
+    console.log("pressed key");
+     });
+
+/////////////
+document.otherEventListener('keypress', respondToKey(event));
+
+function respondToKey(event) {
+    console.log("key pressed.");
+}
+
+// Same but in one line//
+
+document.otherEventListener('keypress', function (event) {
+    console.log("key pressed.");
+    console.log(event);
+}   );
+
 
 
--- Project07 - Object/Solution/drum.js
+++ Project07 - Object/Solution/drum.js
@@ -1,11 +1,63 @@
 
+// Using mouve events to trigger 
+
+var numDrums = document.querySelectorAll('.drum').length;
+for (var choice = 0; choice < numDrums; choice++) {
+    document.querySelectorAll('.drum')[choice].addEventListener("click", handleClick);
+}
 
 function handleClick () {
     
-    console.log(this);    //Show HTML identity from object//
+    console.log(this);    //Show HTML identity from object in document//
     this.style.color = "black";
-    
-    switch (this.innerHTML) {
+    var option = this.innerHTML;
+
+    switch (option) {
+        case "w":
+        var audio = new Audio('sounds/tom-1.mp3');
+        audio.play();
+        break;
+        case "a":
+        var audio = new Audio('sounds/tom-2.mp3');
+        audio.play(); 
+        break;
+        case "s":
+        var audio = new Audio('sounds/tom-3.mp3');
+        audio.play();
+        break;
+        case "d":
+        var audio = new Audio('sounds/tom-4.mp3');
+        audio.play(); 
+        break;
+        case "j":
+        var audio = new Audio('sounds/crash.mp3');
+        audio.play(); 
+        break;
+        case "k":
+        var audio = new Audio('sounds/kick.mp3');
+        audio.play();
+        break;
+        case "l":
+        var audio = new Audio('sounds/snare.mp3');
+        audio.play(); 
+        break;
+
+        default:
+        break;
+    }
+} 
+
+
+
+
+// Using keyboard events to trigger inside whole document
+
+
+document.addEventListener("keypress", playKey);
+
+function playKey() {
+    var option = event.key;
+    switch (option) {
         case "w":
         var audio = new Audio('sounds/tom-1.mp3');
         audio.play();
@@ -39,11 +91,4 @@ function handleClick () {
         break;
     }
 
-} 
-
-var numDrums = document.querySelectorAll('.drum').length;
-for (var choice = 0; choice < numDrums; choice++) {
-    document.querySelectorAll('.drum')[choice].addEventListener("click", handleClick);
-    
 }
-
