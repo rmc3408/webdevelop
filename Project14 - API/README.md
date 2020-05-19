56ba21e4e15eeb1f742c0944f4a1471929405ec9
rmc3408 <rmc3408@protonmail.com>
2020-05-19 11:39:13 -0400

    bitcoin 02

--- Project14 - API/README.md
+++ Project14 - API/README.md
@@ -2,9 +2,10 @@
 
 
 ## ANY API
-https://any-api.com(https://any-api.com) 
+https://any-api.com
 
-#### BITCOIN EXCHANGE
+
+## BITCOIN EXCHANGE
 cURL = to interact(request) a command from specific Client by URL.
 
 ````
@@ -14,3 +15,17 @@ curl https://apiv2.bitcoinaverage.com/convert/global?from=BRL&to=CAD&amount=2
 * URL Codes
 & means AND
 ? means parameters
+
+
+#### Install
+1. npm init
+2. npm install express body-parser
+3. npm install request
+
+
+## REQUEST npm Package
+
+To make a contact to external API call using our express server.
+` LOCAL Server(node/express) <-- API --> External Server `
+Use REQUEST npm module. 
+- npm install request
---
+++ Project14 - API/bitcoin/index.js
@@ -0,0 +1,47 @@
+//jshint esversion: 6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+//TODO: Create port and location
+app.listen(3000, function(){    
+    console.log("server running on port 3000");
+});
+
+
+
+//TODO: Server will get from Browser and RESpond sending url index
+app.get("/", function (req, res){
+    res.sendfile(__dirname+"/index.html");
+
+});
+
+
+
+
+//TODO: From Browser (using the POST info), server will request.
+app.post("/", function(req,res){
+    //console.log(req.body.crypto);
+    //console.log(req.body.fiat);
+
+    //TODO: It will request a JSON with info BTC+USD and other info
+    request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD", function(error, response, body){
+        
+        // It will get response from external server
+        // of one of object key in JSON (statuscode, body).
+        //console.log(response.statuscode);
+
+        console.log(response.body);
+
+    });
+
+
+
+});
