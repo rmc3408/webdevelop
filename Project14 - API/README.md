d01178c0d5b9a8ecffde2715169f74176cc85c86
rmc3408 <rmc3408@protonmail.com>
2020-05-19 20:37:27 -0400

    fixing

--- Project14 - API/README.md
+++ Project14 - API/README.md
@@ -21,15 +21,20 @@ Use REQUEST npm module.
 
 
 
-###### BitcoinExchange website API
+#### BitcoinAverage website API
+https://bitcoinaverage.com/
+
 cURL = to interact(request) a command from specific Client by URL.
 
 ````
 curl https://apiv2.bitcoinaverage.com/convert/global?from=BRL&to=CAD&amount=2
 ````
 
-URL Codes ( & means AND; ? means parameters)
+### In URL Codes 
 
+* After ? means parameters.
+  * key = value
+* & means AND (connect parameters in URL encoded)
 
 
 
--- Project14 - API/bitcoin/index.js
+++ Project14 - API/bitcoin/index.js
@@ -26,37 +26,103 @@ app.get("/", function (req, res){
 
 
 
-//TODO: From Browser (using the POST info), server will request.
-app.post("/", function(req,res){
-    //console.log(req.body.crypto);
-    //console.log(req.body.fiat);
 
 
-    //TODO: It will request a JSON with info BTC+USD and other info
-    request("https://apiv2.bitcoinaverage.com/indices/global/ticker/" + req.body.crypto + req.body.fiat , function(error, response, body) {
-        
-        // It will get response from external server
-        // Get key+value in JSON format (statuscode, body).
-        //console.log(response.statuscode);
-        // console.log(response.body);
 
-        //TODO: Convert from JSON to JS Object and use it.
+
+/******************************************
+ * TODO: Server will request a JSON with info BTC+USD and other info
+                
+    It will get response from external server, 
+    Get key+value in JSON format (statuscode, body).
+                    
+//! REQUEST: this module have two options: URL or Object
+    Get information from URL or Object (JSON to JS Object)
+*  ? To check JSON content -> Check URL on browser!!
+****************************************/
+
+
+
+//! REQUEST From URL
+
+
+
+app.post("/", function(req, res){
+    var moeda = req.body.crypto;
+    var papel = req.body.fiat;
+
+    var baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
+    var finalURL = baseURL + moeda + papel;
+
+
+    request(finalURL, function(error, response, body){
+
         var flatdata = JSON.parse(body);
         var price = flatdata.last;
-        //console.log(price);
 
-        var avgprice = flatdata.averages.week;
-        //console.log(avgprice);
-        
-
+        console.log(response.statuscode);
+        console.log(response.body);
+        console.log(response.body.last);
+       
 
         res.write("<p> The current date is "+ flatdata.display_timestamp + " </p>");
-        res.write("<h1>The price of " + req.body.crypto + 
-        " is "+ price + " " + req.body.fiat +" </h1>");
-
+        res.write("<h1>Crypto:\t" + crypto + "\nPrice:\t" + price + "in " + fiat + " </h1>");
         res.send();
 
     });
+    
+
 
 });
 
+
+
+//! REQUEST From OBJECT
+
+
+
+
+
+
+//TODO: CALCULATE AMOUNT and COVERT
+/**
+app.post("/", function(req, res) {
+    console.log(req.body.crypto);
+    console.log(req.body.fiat);
+
+    var crypto = req.body.crypto; //! crypto in Object class
+    var fiat = req.body.fiat;
+    var amount = req.body.amount;
+    
+    var baseURL = "https://apiv2.bitcoinaverage.com/convert/global";
+
+    var option = {
+        url: baseURL,
+        method: "GET",
+        qs: {
+            from: crypto,  //! crypto in Object class
+            to: fiat,
+            amount: amount
+            }
+    };
+
+
+            
+    
+    var flatdata = JSON.parse(body);  
+    
+    request(option, function(){
+            
+        var price = flatdata.price;
+            
+        res.write("<p> The current date is "+ flatdata.time + " </p>");
+        //res.write("<h1>Crypto:\t" + crypto +
+        //            "\nPrice:\t" + price + 
+        //            "\nFiat:\t" + fiat + 
+        //            "\n\nAmount:\t" + amount + " </h1>");
+        res.send();
+
+    }); 
+
+});
+**/
