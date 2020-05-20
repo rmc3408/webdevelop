dba77a5eba5683fff3d79a4f662ec91305295cb4
rmc3408 <rmc3408@protonmail.com>
2020-05-20 18:05:54 -0400

    EJS 08

--- Project16 - ToDoList/README.md
+++ Project16 - ToDoList/README.md
@@ -1,4 +1,54 @@
-### EJS
+# EJS
 
 
 https://ejs.co/
+
+
+#### Table of contents
+-----------------
+
+- Basic format
+- Delimiters
+- Starting tags
+  - `<%=`: Escaped output
+  - `<%-`: Unescaped output
+  - `<%#`: Comments
+  - `<%`: Scriptlet
+  - `<%_`: Scriptlet, removes all preceding whitespace
+- Ending tags
+  - `%>`: Regular ending tag
+  - `-%>`: Removes trailing newline
+  - `_%>`: Removes all trailing whitespace
+- Literal tags
+- Including other files
+  - “Preprocessor” directive
+  - JavaScript `include()` function
+- Copyright
+
+#### Code JS
+-----------------
+Must be using NPM with express, body-parser and EJS.
+
+1. Create directory views ,and index.ejs inside.
+
+
+```
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function (req, res) {
+    
+    res.render('index', { codeOnHTML_key: newAddItems_Values});
+});
+
+app.post("/", function(req, res){
+    var item = req.body.newItem;
+    newAddItems.push(item);
+    res.redirect("/"); 
+
+});
+
+```
--- Project16 - ToDoList/index.js
+++ Project16 - ToDoList/index.js
@@ -2,30 +2,43 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public")); 
 
-app.listen(3000, function () {  
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.listen(3000, function () {
     console.log("server running on port 3000");
 });
 
+app.set('view engine', 'ejs');
+
+var newAddItems = ["Sleep"];
+
 app.get("/", function (req, res) {
-
     var today = new Date();
-    console.log(today, today.getDay());
-
-    if (today.getDay() == 6 || today.getDay() == 0) {
-        console.log("YEY, weekend");
-        res.sendFile(__dirname + "/index.html");
-
-    }
-    else{
-        console.log("workDay");
-        res.sendFile(__dirname + "/index.html");
-    }
     
+
+    var option = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    };
+
+    var newday = today.toLocaleDateString("pt-BR", option);
+    res.render('index', { sevenDays: newday, task: newAddItems});
+    
+});
+
+app.post("/", function(req, res){
+    
+    item = req.body.newItem;
+    newAddItems.push(item);
+    res.redirect("/"); //** Send taskItem to app.get */
+
+    //? YOU cannot run this, it will miss sevendays in app.get
+    //? -> res.render('index', { task: taskItem });
 
 });
 
