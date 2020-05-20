3f50463a4dfdad744388bf6834b558c51de514d6
rmc3408 <rmc3408@protonmail.com>
2020-05-19 21:15:51 -0400

    fixed

--- Project14 - API/bitcoin/index.js
+++ Project14 - API/bitcoin/index.js
@@ -6,20 +6,22 @@ const request = require('request');
 
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 
 
 //TODO: Create port and location
-app.listen(3000, function(){    
+app.listen(3000, function () {
     console.log("server running on port 3000");
 });
 
 
 
 //TODO: Server will get from Browser and RESpond sending url index
-app.get("/", function (req, res){
-    res.sendfile(__dirname+"/index.html");
+app.get("/", function (req, res) {
+    res.sendfile(__dirname + "/index.html");
 
 });
 
@@ -43,11 +45,17 @@ app.get("/", function (req, res){
 
 
 
+
+
+
+
+
 //! REQUEST From URL
 
 
-
+/**
 app.post("/", function(req, res){
+    
     var moeda = req.body.crypto;
     var papel = req.body.fiat;
 
@@ -59,14 +67,13 @@ app.post("/", function(req, res){
 
         var flatdata = JSON.parse(body);
         var price = flatdata.last;
-
-        console.log(response.statuscode);
+        
         console.log(response.body);
-        console.log(response.body.last);
+        console.log(price);
        
 
         res.write("<p> The current date is "+ flatdata.display_timestamp + " </p>");
-        res.write("<h1>Crypto:\t" + crypto + "\nPrice:\t" + price + "in " + fiat + " </h1>");
+        res.write("<h1> Crypto: " + moeda + " = Price: " + price + " in " + papel + " </h1>");
         res.send();
 
     });
@@ -74,6 +81,10 @@ app.post("/", function(req, res){
 
 
 });
+*/
+
+
+
 
 
 
@@ -81,48 +92,42 @@ app.post("/", function(req, res){
 
 
 
-
-
-
-//TODO: CALCULATE AMOUNT and COVERT
 /**
-app.post("/", function(req, res) {
-    console.log(req.body.crypto);
-    console.log(req.body.fiat);
+//TODO: CALCULATE AMOUNT and COVERT
 
-    var crypto = req.body.crypto; //! crypto in Object class
-    var fiat = req.body.fiat;
+app.post("/", function (req, res) {
+
+    var moeda = req.body.crypto; //! Override crypto in Object class
+    var papel = req.body.fiat;
     var amount = req.body.amount;
-    
-    var baseURL = "https://apiv2.bitcoinaverage.com/convert/global";
 
+    console.log(moeda);
+    console.log(papel);
+
+    var baseURL = "https://apiv2.bitcoinaverage.com/convert/local";
+
+    //! OBJECT CREATE //
     var option = {
         url: baseURL,
         method: "GET",
         qs: {
-            from: crypto,  //! crypto in Object class
-            to: fiat,
+            from: moeda, 
+            to: papel,
             amount: amount
-            }
+        }
     };
 
-
-            
-    
-    var flatdata = JSON.parse(body);  
-    
-    request(option, function(){
-            
+    request(option, function (error, response, body) {
+        var flatdata = JSON.parse(body);
         var price = flatdata.price;
-            
-        res.write("<p> The current date is "+ flatdata.time + " </p>");
-        //res.write("<h1>Crypto:\t" + crypto +
-        //            "\nPrice:\t" + price + 
-        //            "\nFiat:\t" + fiat + 
-        //            "\n\nAmount:\t" + amount + " </h1>");
-        res.send();
 
-    }); 
+        console.log(price);
+
+        //res.write("<p> The current date is " + flatdata.time + " </p>");
+        //res.write("<h1>" + amount + " " + moeda + " = " + price + " " + papel + " </h1>");
+        //res.send();
+
+    });
 
 });
-**/
+*/
