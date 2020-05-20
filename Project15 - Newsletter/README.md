e8f274686422b4faa038cb21366491a04c181c31
rmc3408 <rmc3408@protonmail.com>
2020-05-20 01:11:54 -0400

    ok

--- Project15 - Newsletter/README.md
+++ Project15 - Newsletter/README.md
@@ -2,5 +2,31 @@
 
 ### MailChimp
 
-API KEY:   f0679f27a55fd90412b5ef954423874b-us18
-Audience ID:  f6f5e44c55.
+* API KEY:   f0679f27a55fd90412b5ef954423874b-us18
+* Audience ID:  f6f5e44c55.
+
+
+### Basic HTTP authorization - any API
+``` 
+var username = "rmc3408";
+    var password = "f0679f27a55fd90412b5ef954423874b-us18";
+    var auth = username + " " + password;
+
+ var option = {
+        url: 'https://us18.api.mailchimp.com/3.0/lists/f6f5e44c55',
+        method: 'POST',
+        headers: {                  
+            "Authorization": auth}
+    };
+
+    request(option, function (error, response, body) {
+        if (error) {
+            console.log(error);
+
+        } else {
+            console.log(response.statusCode);
+            console.log('body : ', body);
+        }
+
+});
+```
--- Project15 - Newsletter/app.js
+++ Project15 - Newsletter/app.js
@@ -5,13 +5,15 @@ const bodyParser = require('body-parser');
 const request = require('request');
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.static("public")); //folder where all local files
 // CSS and images MUST be in subfolders.
 
 ////////////////////////////
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log("server running on port 3000");
 });
 
@@ -19,17 +21,21 @@ app.listen(3000, function() {
 
 
 /////////////////////////////////
-app.get("/", function(req, res){
-    res.sendFile(__dirname+"/signup.html");
-    
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
+
 
 });
 
 ////////////////////////
-app.post("/", function(req, res){
+app.post("/", function (req, res) {
     var fname = req.body.first;
     var lname = req.body.last;
     var email = req.body.email;
+
+    var username = "rmc3408";
+    var password = "f0679f27a55fd90412b5ef954423874b-us18";
+    var auth = username + " " + password;
 
     console.log(fname, lname, email);
     //res.send("<h2>Full Name: "+ fname +" "+ lname + " ,email: "+email+ "</h2>");
@@ -37,20 +43,19 @@ app.post("/", function(req, res){
     var option = {
         url: 'https://us18.api.mailchimp.com/3.0/lists/f6f5e44c55',
         method: 'POST',
-        user: 'rmc3408:f0679f27a55fd90412b5ef954423874b-us18'
+        headers: {                  //! Basic HTTP authorization
+            "Authorization": auth}
     };
 
-    request(option, function (error, response, body){
-        if(error){
+    request(option, function (error, response, body) {
+        if (error) {
             console.log(error);
 
-        }else{
+        } else {
             console.log(response.statusCode);
+            console.log('body : ', body);
         }
 
     });
 
 });
-
-
-
